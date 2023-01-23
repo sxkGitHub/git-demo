48b71de45d4da0ef52ad788bc709febefd331baf
shenxiaokang <1263812921@qq.com>
2023-01-23 19:40:05 +0800

    高级SQL

--- sqlList.sql
+++ sqlList.sql
@@ -1,5 +1,5 @@
 select * from Province;
 select * from Province join City using(ProvinceId);
-
+select concat(ProvinceName,CityName) from Province join City on Province.ProvinceId = City.ProvinceId
 
 
